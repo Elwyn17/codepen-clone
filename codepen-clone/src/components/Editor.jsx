f6410b7dd7a6b8097303e593f77dab5721a726b3
Anurag Elwyn Xess <anuragelwyn@gmail.com>
2023-09-09 11:00:10 +0530

    styled editor

--- codepen-clone/src/components/Editor.jsx
+++ codepen-clone/src/components/Editor.jsx
@@ -1,15 +1,42 @@
-import { Box } from "@mui/material";
+import { Box, styled} from "@mui/material";
+import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
 import React from "react";
+
+const CustomizedHeading = styled(Box)`
+    background: #1d1e22;
+    display: flex;
+    padding: 9px 12px;
+`;
+
+const CustomizedHeader = styled(Box)`
+    display: flex;
+    background: #060606;
+    color: #aaaebc;
+    justify-content: space-between;
+    font-weight: 700;
+`;
 
 function Editor() {
     return (
         <Box>
-            <Box>
-                <Box>
-                    <Box component="span"></Box>
+            <CustomizedHeader>
+                <CustomizedHeading>
+                    <Box component="span"
+                    style={{
+                        background: 'red',
+                        height: 20,
+                        width: 20,
+                        display: 'flex',
+                        placeContent: 'center',
+                        borderRadius: 5,
+                        marginRight: 5,
+                        paddingBottom: 2
+                    }}
+                    >/</Box>
                     HTML
-                </Box>
-            </Box>
+                </CustomizedHeading>
+                <CloseFullscreenIcon />
+            </CustomizedHeader>
             <Box></Box>
         </Box>
     );
